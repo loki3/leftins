391457bec6df350d1b49e5052c10156f76350c30
Scott Sherman <scott@loki3.com>
2014-01-11 13:29:49 -0800

    Delete leftins.js

--- leftins.js
+++
@@ -1,3 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
-goog.addDependency("../leftins/core.js", ['leftins.core'], ['cljs.core']);
